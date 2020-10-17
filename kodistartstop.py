c421fa10ee20e2515bb07b6d0030adc8a85a5743
simonfelding <45149055+simonfelding@users.noreply.github.com>
2020-10-17 17:09:48 +0200

    final fixes, now it doesn't restart the tv.

--- kodistartstop.py
+++ kodistartstop.py
@@ -2,36 +2,54 @@
 
 # Kodi start-stop script 
 
-import cec,time,os
+import cec,time,os,sys,psutil
+
+def logger(str):
+    if "-v" in sys.argv[1:]:
+        print(str)
+    else:
+        pass
 
 def listener(a,b,c,message):
     global on
     if '>> 0f:36' == message:
         on = 0
+    if tv.is_on():
+        on = 1
 
 def init():
     cec.init()
     global tv
     tv = cec.Device(0)
     global on
-    if tv.is_on():
-    	on = 1 # avoids polling the tv all the time and causing libCEC errors.
-    else:
+    try:
+        if tv.is_on():
+            on = 1 # avoids polling the tv all the time and causing libCEC errors.
+        else:
+            on = 0
+    except:
         on = 0
     cec.add_callback(listener, cec.EVENT_LOG)
 
 # relaunched by systemctl to evade a CEC bug with Kodi, otherwhise put this part in a while true loop.
 def main():
+    logger("initialising")
     init()
+    logger("finished init")
 
     while on == 0: # wait for TV on
-        time.sleep(1)
+        time.sleep(0.1)
+    logger("tv is on, starting kodi")
     os.system("systemctl start kodi")
-
-    while on == 1: # wait for TV off
-        time.sleep(1)
+    while on == 1:
+        if "kodi" not in (p.name() for p in psutil.process_iter()): # wait for TV off/kodi exit
+            os.system("systemctl start kodi")
+        time.sleep(0.5)
+    logger("tv is off, stopping kodi")
     os.system("systemctl stop kodi")
-    time.sleep(30) # waiting for kodi to finish
-
+    while tv.is_on() or "kodi" in (p.name() for p in psutil.process_iter()):
+        logger("waiting for kodi and tv to be off")
+        time.sleep(0.5)
+    logger("now the whole thing is off.")
 if __name__ == "__main__":
     main()
