f3b96ed9ff56f16a6844bbc5eefdfad24476de63
simonfelding <45149055+simonfelding@users.noreply.github.com>
2021-04-21 19:40:15 +0200

    Update kodistartstop.py

--- kodistartstop.py
+++ kodistartstop.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-# Kodi start-stop script 
+# Kodi start-stop script
 
-import cec,time,os,sys,psutil
+import cec,time,os,sys,psutil,syslog
 
 def logger(str):
+    global verbose
     if "-v" in sys.argv[1:]:
         print(str)
     else:
-        pass
+        syslog.syslog(f"kodistartstop: {str}")
 
 def listener(a,b,c,message):
     global on
@@ -33,20 +34,25 @@ def init():
 
 # relaunched by systemctl to evade a CEC bug with Kodi, otherwhise put this part in a while true loop.
 def main():
+    if "-v" in sys.argv[1:]:
+        print("-v in arguments, logging to stdout only")
     logger("initialising")
     init()
     logger("finished init")
 
     while on == 0: # wait for TV on
         time.sleep(0.1)
-    logger("tv is on, starting kodi")
+    logger("tv is on, starting bluetooth and kodi")
+    os.system("systemctl start pulseaudio-bluetooth")
     os.system("systemctl start kodi")
     while on == 1:
         if "kodi" not in (p.name() for p in psutil.process_iter()): # wait for TV off/kodi exit
+            logger("kodi process not running but tv is on. restarting kodi..")
             os.system("systemctl start kodi")
         time.sleep(0.5)
-    logger("tv is off, stopping kodi")
+    logger("tv is off, stopping kodi and bluetooth")
     os.system("systemctl stop kodi")
+    os.system("systemctl stop pulseaudio-bluetooth")
     while tv.is_on() or "kodi" in (p.name() for p in psutil.process_iter()):
         logger("waiting for kodi and tv to be off")
         time.sleep(0.5)
